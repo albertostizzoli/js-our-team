0d1f55f63609f3e46cc54f45a677fc817b0c6e92
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-23 14:31:22 +0200

    Complete HTML and Array of Objects

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// creo un array di oggetti che rappresenta i membri del team
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Carroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+];
